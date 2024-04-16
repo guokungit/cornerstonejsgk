c032e9ff99e3cc57fabec949cdee53385b5ef86a
goodkeeper <guo2401143699@qq.com>
2024-04-16 13:43:28 +0800

    tets

--- packages/tools/src/tools/PanTool.ts
+++ packages/tools/src/tools/PanTool.ts
@@ -23,7 +23,7 @@ class PanTool extends BaseTool {
   }
 
   mouseDragCallback(evt: EventTypes.InteractionEventType) {
-    console.log('mouseDragCallback   pan')
+    console.log('mouseDragCallback test   pan');
     this._dragCallback(evt);
   }
 
